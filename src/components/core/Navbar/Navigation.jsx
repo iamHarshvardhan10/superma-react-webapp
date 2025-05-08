8df5b9b116343e751d425d855e67e809625d1fc1
Harshvardhan <shreeharshpb11@gmail.com>
2025-05-08 23:32:22 +0530

    create navbar UI

--- src/components/core/Navbar/Navigation.jsx
+++ src/components/core/Navbar/Navigation.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import './navbar.scss'
+import React from "react";
+import "./navbar.scss";
+import logo from "../../../assets/asset 0.svg";
+import { Button } from "../../../components/ui/button";
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <nav>
+      <img src={logo} alt="" />
+      <ul>
+        <li>Home</li>
+        <li>Blogs</li>
+        <li>Changelog</li>
+        <li>Waitlist</li>
+      </ul>
+      <div className="btn">
+        <Button>Contact Us</Button>
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
